6529474560b97f62901041d4878c8e8484cb78c4
HojatS <49037510+HojatS@users.noreply.github.com>
2022-11-07 20:11:18 -0800

    Update TripleSDES.java

--- Final Version (One project)/TripleSDES.java
+++ Final Version (One project)/TripleSDES.java
@@ -4,151 +4,33 @@ import java.util.*;
 public class TripleSDES {
     
     public static void main(String[] args){
-        
-        byte[] input = {1,1,0,1,0,1,1,1};
+        byte[] input = {1,1,1,0,0,1,1,0};
         byte[] k1 = {1,0,0,0,1,0,1,1,1,0};
         byte[] k2 = {0,1,1,0,1,0,1,1,1,0};
-        
         //encrypt
-        byte[] a = encrypt(k1,decrypt(k2,encrypt(k1,input)));
-        
+        byte[] a = encrypt(k1,k2,input);
         //decrypt
-        //byte[] a = decrypt(k1,encrypt(k2,decrypt(k1,input)));
-        
-        //System.out.println("RESULT:");
-        //Utl.print(a);
-        displayTable();
-        
+        //byte[] a = decrypt(k1,k2,input);
+        System.out.println("RESULT:");
+        Utl.print(a);
+        //displayTable();
     }
     
     //encrypt
-    public static byte[] encrypt(byte[] rawKey, byte[] plainText){
+    public static byte[] encrypt(byte[] rawKey1,byte[] rawKey2, byte[] plainText){
         byte[] cipher = new byte[plainText.length];
-        
-        //IP (initial permutation)
-        int[] IP = {2, 6, 3, 1, 4, 8, 5, 7};
-        byte[] IPResult = Utl.permutation(plainText, IP);
-        
-        //Generate Keys
-        KeyGen keyGen = new KeyGen(rawKey);
-        byte[] K1 = keyGen.getK1();
-        byte[] K2 = keyGen.getK2();
-        
-        //System.out.println("K1:");
-        //Utl.print(K1);
-        //System.out.println("K2:");
-        //Utl.print(K2);
-        
-        //fK round 1
-        cipher = fK(IPResult, K1);
-        
-        //Swap = left shift by 4
-        
-        cipher = Utl.leftShift(cipher, cipher.length/2);
-        
-        //fk round 2
-        cipher = fK(cipher, K2);
-        
-        
-        //IP-1 (inverse)
-        int[] inverseIP = {4, 1, 3, 5, 7, 2, 8, 6};
-        cipher = Utl.permutation(cipher, inverseIP);
-        
+        cipher = SDES.encrypt(rawKey1,SDES.decrypt(rawKey2,SDES.encrypt(rawKey1,plainText)));
         return cipher;
     }
     
     //decrypt
-    public static byte[] decrypt(byte[] rawKey, byte[] cipherText){
+    public static byte[] decrypt(byte[] rawKey1,byte[] rawKey2, byte[] cipherText){
         byte[] plaintext = new byte[cipherText.length];
-        
-        //IP (initial permutation)
-        int[] IP = {2, 6, 3, 1, 4, 8, 5, 7};
-        byte[] IPResult = Utl.permutation(cipherText, IP);
-        
-        //generate keys
-        KeyGen keyGen = new KeyGen(rawKey);
-        byte[] K1 = keyGen.getK1();
-        byte[] K2 = keyGen.getK2();
-        
-        //System.out.println("K1:");
-        //Utl.print(K1);
-        //System.out.println("K2:");
-        //Utl.print(K2);
-        
-        //fK round 2
-        plaintext = fK(IPResult, K2);
- 
-        //Swap = left shift by half length
-        plaintext = Utl.leftShift(plaintext, plaintext.length/2); 
-        
-        //fk round 1
-        plaintext = fK(plaintext, K1);
-        
-        //IP-1 (inverse)
-        int[] inverseIP = {4, 1, 3, 5, 7, 2, 8, 6};
-        plaintext = Utl.permutation(plaintext, inverseIP);
-        
+        plaintext = SDES.decrypt(rawKey1,SDES.encrypt(rawKey2,SDES.decrypt(rawKey1,cipherText)));
         return plaintext;
-        
     }
     
-    //fK function
-    public static byte[] fK(byte[] input, byte[] key){
-        byte[] output = new byte[input.length];
-        
-        //split 
-        int splitSize = input.length/2;
-        byte[] IPLeft = Arrays.copyOfRange(input, 0, splitSize);
-        byte[] IPRight = Arrays.copyOfRange(input, splitSize, input.length);
-        
-        output = ArrayUtils.addAll(Utl.XOR(IPLeft,functionF(IPRight,key)),IPRight);
-        
-        return output;
-    }
-    
-    //F Function
-    public static byte[] functionF(byte[] byteArray, byte[] key){
-        byte[] result = new byte[byteArray.length];
-        
-        //EP (expansion Pbox)
-        int[] EP = {4, 1, 2, 3, 2, 3, 4, 1};
-        result = Utl.permutation(byteArray, EP);
-        
-        //XOR
-        result = Utl.XOR(result, key);
-        
-        //split
-        byte[] resultLeft = Arrays.copyOfRange(result, 0, result.length/2);
-        byte[] resultRight = Arrays.copyOfRange(result, result.length/2, result.length);
-        
-        //S-boxes
-        int[][] S0 = { {1,0,3,2} , {3,2,1,0} , {0,2,1,3} , {3,1,3,2} } ;
-        int[][] S1 = { {0,1,2,3},  {2,0,1,3}, {3,0,1,0}, {2,1,0,3}} ;
-        
-        byte[] row0array = {resultLeft[0], resultLeft[3]};
-        byte[] column0array = {resultLeft[1], resultLeft[2]};
-        
-        byte[] row1array = {resultRight[0], resultRight[3]};
-        byte[] column1array = {resultRight[1], resultRight[2]};
-        
-        int row0 = Utl.toDec(row0array);
-        int column0 = Utl.toDec(column0array);
-        
-        int row1 = Utl.toDec(row1array);
-        int column1 = Utl.toDec(column1array);
-        
-        resultLeft = Utl.toByteArray(S0[row0][column0],2);
-        resultRight = Utl.toByteArray(S1[row1][column1],2);
-        
-        result = ArrayUtils.addAll(resultLeft, resultRight);
-        
-        //P4
-        int[] P4 = {2, 4, 3, 1};
-        result = Utl.permutation(result, P4);
-        
-        return result;
-        
-    }
+
     public static void displayTable(){
         ArrayList<byte[]> rawKeyList1 = new ArrayList<byte[]>();
         ArrayList<byte[]> rawKeyList12 = new ArrayList<byte[]>();
@@ -220,15 +102,15 @@ public class TripleSDES {
         rawKeyList22.add(rk16);
         cypherTextList.add(ct4);
         
-        byte[] answera = encrypt(rk1, decrypt(rk2,encrypt(rk1,pt1)));
-        byte[] answerb = encrypt(rk3, decrypt(rk4,encrypt(rk3,pt2)));
-        byte[] answerc = encrypt(rk5, decrypt(rk6,encrypt(rk5,pt3)));
-        byte[] answerd = encrypt(rk7, decrypt(rk8,encrypt(rk7,pt4)));
+        byte[] answera = encrypt(rk1, rk2,pt1);
+        byte[] answerb = encrypt(rk3,rk4,pt2);
+        byte[] answerc = encrypt(rk5,rk6,pt3);
+        byte[] answerd = encrypt(rk7,rk8,pt4);
 
-        byte[] answere = decrypt(rk9,encrypt(rk10,decrypt(rk9,ct1)));
-        byte[] answerf = decrypt(rk11,encrypt(rk12,decrypt(rk11,ct2)));
-        byte[] answerg = decrypt(rk13,encrypt(rk14,decrypt(rk13,ct3)));
-        byte[] answerh = decrypt(rk15,encrypt(rk16,decrypt(rk15,ct4)));
+        byte[] answere = decrypt(rk9,rk10,ct1);
+        byte[] answerf = decrypt(rk11,rk12,ct2);
+        byte[] answerg = decrypt(rk13,rk14,ct3);
+        byte[] answerh = decrypt(rk15,rk16,ct4);
         answerList.add(answera);
         answerList.add(answerb);
         answerList.add(answerc);
@@ -239,8 +121,8 @@ public class TripleSDES {
         answerList2.add(answerg);
         answerList2.add(answerh);
 
-        System.out.println("\t\tRawkey 1\t\t\t\t\t\t\t\tRawkey 2\t\t\t\t\tPlaintext Text" +
-                "\t\t\t\t\tCipher Text");
+        System.out.println("\t\tRawkey 1\t\t\t\tRawkey 2\t\t\tPlaintext Text" +
+                "\t\t\tCipher Text");
 
         for(int i = 0; i < answerList.size(); i++){
             printArrayList(rawKeyList1.get(i),rawKeyList2.get(i), plainTextList.get(i),answerList.get(i));
@@ -256,7 +138,7 @@ public class TripleSDES {
                 Arrays.toString(plaintext) + "\t\t" + Arrays.toString(answer));
     }
     
-    
 }
+
 
 
